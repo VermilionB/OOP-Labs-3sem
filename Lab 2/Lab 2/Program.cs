3ae6c4540e0313242960a9709021dc23ba4cc937
Ilya <serezha.mostyko@mail.ru>
2021-09-14 16:49:53 +0300

    first commit in 2 lab

---
+++ Lab 2/Lab 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Bool = false;
+            byte Byte = 129;
+            sbyte sByte = -120;
+            char Char = 'X';
+            decimal Decimal = 1.7M;
+            double Double = 1.72;
+            float Float = 1.7f;
+            int Int = -1;
+            uint uInt = 15;
+
+        }
+    }
+}
