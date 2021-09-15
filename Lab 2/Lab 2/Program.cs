3e51f069a1a6471cc62dcca32eb42240623b49b6
Ilya <serezha.mostyko@mail.ru>
2021-09-15 17:09:37 +0300

    init types and console write

---
+++ Lab 2/Lab 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Bool = true;
+            byte Byte = 250;            // [0, 255]
+            sbyte sByte = -120;         // [-128, 127]
+            char Char = 'A';
+            decimal Decimal = 1.76M;    // Precision 28-29 digits
+            double Double = 1.72;       // Precision 15-17 digits
+            float Float = 1.72f;        // Precision 6-9 digits
+            int Int = 1;
+            uint uInt = 1;
+            long Long = 9223372036854775807; // [-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807]
+            ulong uLong = 9223372036854775808;
+            short Short = -32000;
+            ushort uShort = 64000;
+
+            Console.WriteLine($"Bool: {Bool}\nByte: {Byte}\nsByte: {sByte}\nChar: {Char}\nDecimal: {Decimal}\n" +
+                $"Double: {Double}\nFloat: {Float}\nInt: {Int}\nuInt: {uInt}\nLong: {Long}\nuLong: {uLong}\n" +
+                $"Short: {Short}\nuShort: {uShort}");
+        }
+    }
+}
