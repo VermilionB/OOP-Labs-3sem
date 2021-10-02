781eeaf55a4126af6d509eecc661a151712004d6
Ilya <serezha.mostyko@mail.ru>
2021-10-02 14:54:58 +0300

    test change

---
+++ Laba 3/Laba 3/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba_3
+{
+    class Student
+    {
+        private int id;
+        private string surname;
+        private string name;
+        private string patronymic;
+
+    }
+}
